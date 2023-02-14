05bd003bbfec9ef4825f5840b384448fdb7cc030
Живко Маджев <z.madzhev@consultcommerce.com>
2023-02-14 22:09:55 +0200

    Migrate to azure database

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace WebAppChat
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("Hermess")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
